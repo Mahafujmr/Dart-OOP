1fe8e5b6600c95e0d55d25d4fef703541c694bea
Mahafujmr <mrtuhin444@gmail.com>
2024-11-19 17:21:40 +0530

    dart class

--- dart_class/class.dart
+++ dart_class/class.dart
@@ -1,3 +1,35 @@
 // dart class
 // class store variable , function, method
 // class part of software
+
+class mobile {
+
+  // class properties
+  var name ='Realme';
+  var price = 30000;
+  var camera = 20;
+  // var name ,var price , var  camera = is a Instance Variable
+
+  // create  methods
+ void display(){
+   print('my mobile name is $name');
+   print('Mobile price is $price');
+   print('This mobile camera is $camera');
+
+ }
+  void person(){
+    var a = 22;
+    var b =10;
+    print(a+b);
+  }
+}
+
+// access class
+void main(){
+  // create new object = obj
+  var obj = mobile();
+  // access the method form class
+  obj.display();
+  // access the method form class
+  obj.person();
+}
