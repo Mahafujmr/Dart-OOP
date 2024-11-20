77599b300f83bbb56cf03ad8eef5635a4bad711c
Mahafujmr <mrtuhin444@gmail.com>
2024-11-20 19:24:23 +0530

    dart encapsulation

---
+++ encapsulation/encapsulation_syntax.dart
@@ -0,0 +1,49 @@
+// dart oop - encapsulation syntax and all detalis
+// dart encapsulation
+// encapsulation use data set and get
+// data set= variable value set
+// data get = variable value get
+
+/*
+to Set value in variable
+to Get value from variable
+to make encapsulation class
+to make data model
+ */
+
+/*
+/// Dart encapsulation syntax
+class example{
+  // _age = private variable
+  // not access directly private variable
+  int ? _age;
+
+  // _age = variable is a private variable
+  // variable or method private then use = _ under score
+
+  // first set and then get
+
+  set setAge(int a){
+    _age =a;
+
+    // not allow directly access = private variable ( _age)
+    // any private value access then - use to setter and getter
+
+
+  }
+  get getAge {
+    return _age;
+    // sometimes any sensitive  data or variable-private
+  }
+}
+
+
+
+// dart - public or private
+// encapsulation use = setter and getter
+
+// first private variable set then private variable get
+// set set(variable name)= set setAge
+// get get(variable name) = get getAge // then return
+
+ */
