75ec3343f2be1962c70364691a63c6d3aa9aa640
Mahafujmr <mrtuhin444@gmail.com>
2024-11-21 21:19:39 +0530

    polymorphism_01

--- dart_polymorphism/problem_02.dart
+++ dart_polymorphism/problem_02.dart
@@ -1,1 +1,31 @@
 // dart polymorphism
+
+class employer{
+  void salary(){
+    print('Employer salary is ${1000}');
+  }
+
+}
+
+class manager extends employer {
+  @override
+  void salary() {
+    print('Manager salary is \$5000');
+  }
+}
+
+class ceo extends employer{
+  @override
+  void salary() {
+    print('Ceo salary is \$5500');
+  }
+}
+
+void main(){
+  employer Employee;
+  Employee= manager();
+  Employee.salary();
+
+  Employee =ceo();
+  Employee.salary();
+}
