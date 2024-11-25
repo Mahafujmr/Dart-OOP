d02d79825fe29a12fca31c48d1ff8de45a1b106c
Mahafujmr <mrtuhin444@gmail.com>
2024-11-25 20:41:43 +0530

    dart abstract

--- dart_abstract_class/abstract_class/abstract_example.dart
+++ dart_abstract_class/abstract_class/abstract_example.dart
@@ -2,30 +2,40 @@
 abstract class Animal{
   // creation abstract method
   void makeSound();
+  // no implementation
 
   // creation normal method or regular method
 void eat(){
   print('This animal is eating....');
 }
 }
-
+// concrete class //Inheriting the abstract class
 class dog extends Animal{
+
+  // override the abstract method
 @override
   void makeSound() {
   print('Dog Says: Woof!, Woof!');
   }
 }
-
+// concrete class // inheriting the abstract class
 class cat extends Animal{
+  // override the abstract method
   @override
   void makeSound() {
     print('Cat Says: Meow!, Meow!');
   }
 }
 
+//main function
 void main(){
+  // not creation abstract class object
+ // Animal animal =Animal(); // this is abstract class
+  // compile time error
+
+  //dog = class // Dog = object
   dog Dog =dog();
-  Dog.eat();
+  Dog.eat(); // acce
   Dog.makeSound();
 
 
